3595860928943d52bd15f8a50068fb59a019084a
MJtsholo <tsholofelo.mautjana@umuzi.org>
2019-04-10 15:16:22 +0200

    2nd commit

--- factorial.py
+++ factorial.py
@@ -1,10 +1,9 @@
 
-n = eval(input("Please enter a whole number: "))
-fact = 1
+
 
 def factorial():
-	global fact
-	global n
+	n = int(input("Please enter a whole number: "))
+	fact = 1
 	if n < 0:
 		print("Sorry, factorial does not exist for negative numbers")
 	elif n == 0:
@@ -17,13 +16,24 @@ def factorial():
 def not_number():
 	while True:
 		try:
-			n = eval(input("Please enter a whole number: "))
+			n = int(input("Please enter a whole number: "))
 			if type(n) == int :
-				return ValueError
-		except ValueError:
+				#n = int(input("Please enter a whole number: "))
+				fact = 1
+				if n < 0:
+					print("Sorry, factorial does not exist for negative numbers")
+				elif n == 0:
+				   print("The factorial of 0 is 1")
+				else:
+				   for n in range(1,n + 1):
+				       fact = fact * n
+				   print("The factorial of",n, "is ",fact)
+			#	return ValueError
+			#factorial()
+		except:
 			print('That is not a number!')
 
-factorial()
+#factorial()
 not_number()		
 
 
